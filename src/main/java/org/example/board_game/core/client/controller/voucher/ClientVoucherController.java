292d5e188a5047dc8c783d2a2ea52e66c6e41a26
LeVan-Duy <levanduy1032001@gmail.com>
2025-03-14 18:01:26 +0700

    fix

--- src/main/java/org/example/board_game/core/client/controller/voucher/ClientVoucherController.java
+++ src/main/java/org/example/board_game/core/client/controller/voucher/ClientVoucherController.java
@@ -1,31 +1,31 @@
-package org.example.board_game.core.client.controller.voucher;
-
-import lombok.AccessLevel;
-import lombok.experimental.FieldDefaults;
-import org.example.board_game.core.admin.domain.dto.request.voucher.AdminVoucherRequest;
-import org.example.board_game.core.admin.domain.dto.response.voucher.AdminVoucherResponse;
-import org.example.board_game.core.admin.service.voucher.AdminVoucherService;
-import org.example.board_game.core.common.base.BaseController;
-import org.example.board_game.utils.Response;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/admin/voucher")
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class ClientVoucherController extends BaseController<AdminVoucherResponse, String, AdminVoucherRequest> {
-
-    AdminVoucherService adminVoucherService;
-
-    public ClientVoucherController(AdminVoucherService service) {
-        super(service);
-        this.adminVoucherService = service;
-    }
-
-    @PostMapping("/deactivate/{id}")
-    public Response<Object> deactivate(@PathVariable("id") String id) {
-        return adminVoucherService.deactivateVoucher(id);
-    }
-}
+//package org.example.board_game.core.client.controller.voucher;
+//
+//import lombok.AccessLevel;
+//import lombok.experimental.FieldDefaults;
+//import org.example.board_game.core.admin.domain.dto.request.voucher.AdminVoucherRequest;
+//import org.example.board_game.core.admin.domain.dto.response.voucher.AdminVoucherResponse;
+//import org.example.board_game.core.admin.service.voucher.AdminVoucherService;
+//import org.example.board_game.core.common.base.BaseController;
+//import org.example.board_game.utils.Response;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/admin/voucher")
+//@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+//public class ClientVoucherController extends BaseController<AdminVoucherResponse, String, AdminVoucherRequest> {
+//
+//    AdminVoucherService adminVoucherService;
+//
+//    public ClientVoucherController(AdminVoucherService service) {
+//        super(service);
+//        this.adminVoucherService = service;
+//    }
+//
+//    @PostMapping("/deactivate/{id}")
+//    public Response<Object> deactivate(@PathVariable("id") String id) {
+//        return adminVoucherService.deactivateVoucher(id);
+//    }
+//}
